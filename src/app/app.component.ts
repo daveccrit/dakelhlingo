bd8d5c0cdea35d6e96a0102af2d91e46caad8db3
David Critchley <dcritchley@mozilla.com>
2018-12-03 23:34:17 -0800

    Remove old project files

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: `app.component.html`,
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
-})
-export class AppComponent {}
--- src/app/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'home',
-  templateUrl: `home.component.html`,
-  styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
-})
-export class HomeComponent {}
