e74d503e7c4a5af1cf4b8b9c566b7988f699b13f
David Critchley <dcritchley@mozilla.com>
2019-02-13 00:14:52 -0800

    adds lesson page and changes navigation model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LessonCategoriesComponent } from './lesson-categories/lesson-categories.component';
 import { LessonsComponent } from './lessons/lessons.component';
+import { LessonComponent } from './lesson/lesson.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'lesson-categories', component: LessonCategoriesComponent },
-  { path: 'lessons', component: LessonsComponent }
+  { path: 'lessons/:catid', component: LessonsComponent },
+  { path: 'lesson/:catid/:lessonid', component: LessonComponent }
 ];
 
 @NgModule({
--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,4 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+type LessonCategoryArrayType = Array<{
+  id: number;
+  icon: string;
+  textDakelh: string;
+  textEnglish: string;
+  isComplete?: boolean;
+}>;
+
+interface LessonCategoryType {
+  id: number;
+  icon: string;
+  textDakelh: string;
+  textEnglish: string;
+  isComplete?: boolean;
+}
+
+const categoriesDb: LessonCategoryArrayType = [
+  {
+    id: 1,
+    icon: 'color_lens',
+    textDakelh: 'Ndidot\'en',
+    textEnglish: 'What does it look like?'
+  },
+  {
+    id: 2,
+    icon: 'access_time',
+    textDakelh: 'Ntsoya oozulh ',
+    textEnglish: 'What time is it?'
+  },
+  {
+    id: 3,
+    icon: 'date_range',
+    textDakelh: 'Yusk\'ut ooza\'',
+    textEnglish: 'Time of year'
+  },
+  {
+    id: 4,
+    icon: 'pets',
+    textDakelh: 'Khuna-i ',
+    textEnglish: 'That which lives (large fur animals)'
+  }
+];
 
 @Component({
   selector: 'app-lessons',
@@ -6,10 +51,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lessons.component.scss']
 })
 export class LessonsComponent implements OnInit {
+  lessonCategory: LessonCategoryType;
+  categories = categoriesDb;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getLessons();
   }
 
+  getLessons(): void {
+    const catId = +this.route.snapshot.paramMap.get('catid');
+    this.lessonCategory = this.categories.find(
+      category => category.id === catId
+    );
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
